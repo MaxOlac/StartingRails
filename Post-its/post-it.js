4b20de404372bbcdc64289fb863d609a9c33a9ac
codeamac07 <codeamac07@gmail.com>
2016-12-05 16:41:52 -0600

    modify ti be in OOP

--- Post-its/post-it.js
+++ Post-its/post-it.js
@@ -14,22 +14,23 @@ $( document ).ready(function() {
     initialize();
   };
 
-  var PostIt = function(x_pos,y_pos) {
+  var PostIt = function() {
     // Aquí va el código relacionado con un post-it
-    var master = $('<div id="master"class="post-it"></div>');
-    master.appendTo("#board");
-    var header = $('<div class="header"></div>');
-    header.appendTo(master);
-    master.draggable({
-      containment: '#board',
-      handle :header,
-      stack: '#master',
-    });
-    $('<div class="close">X</div>').appendTo(header);
-    $('<div class="content" contenteditable="true">...</div>').appendTo(master)
-    master.css("left",x_pos+'px')
-    master.css("top",y_pos+'px')
-
+    this.create = function(x_pos,y_pos){
+      var master = $('<div id="master"class="post-it"></div>');
+      master.appendTo("#board");
+      var header = $('<div class="header"></div>');
+      header.appendTo(master);
+      master.draggable({
+        containment: '#board',
+        handle :header,
+        stack: '#master',
+      });
+      $('<div class="close">X</div>').appendTo(header);
+      $('<div class="content" contenteditable="true">...</div>').appendTo(master)
+      master.css("left",x_pos+'px');
+      master.css("top",y_pos+'px');
+    }
   };
 
   $(function() {
@@ -39,16 +40,17 @@ $( document ).ready(function() {
 //==================================================================
   $("#board").on('click','.close',function(event){
     event.stopPropagation();
-    var post = $(this).parent().parent();
+    var post= $(this).parent().parent();
     post.remove();
-    // $(this).remove();
   });
+
 //==================================================================
 $( "#board" ).dblclick(function() {
   if(event.target.nodeName === "BODY"){
     var pageX = event.pageX;
     var pageY = event.pageY;
-    new PostIt(pageX,pageY);
+    var post_it = new PostIt();
+    post_it.create(pageX,pageY);
   };
  });
 //==================================================================
